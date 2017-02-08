43cd9ed5408aa55e013093f8849594af60a9d47c
Wei Liu <liuweiself@126.com>
2017-02-08 08:23:25 +0800

    commit

---
+++ Bit/Bit/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Bit
+//
+//  Created by 刘微 on 2017/2/8.
+//  Copyright © 2017年 WayneInc. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
